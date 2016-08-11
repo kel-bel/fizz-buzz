4d320b9aac15bd83eeceff0d3ee093e7dfd30027
Kelly Ceriani <Kelly Ceriani>
2016-08-11 14:03:08 -0400

    js/

--- js/script.js
+++ js/script.js
@@ -1,12 +1,20 @@
-for (var i = 1; i <= 100; i++) {
-	if ( (i % 3) == 0 ) {
+var num = prompt("Select a number", "1-100");
+
+function playFizzbuzz(number) {
+for (var i = 1; i <= number; i++) {
+	if ( (i % 5) == 0 && (i % 3) == 0) {
+		console.log('fizzbuzz')
+	}
+	else if ( (i % 3) == 0 ) {
 		console.log('fizz')
-	};	
+	}	
 	else if ( (i % 5) == 0) {
 		console.log('buzz')
-	};
-	else if ( (i % 5) == 0, (i % 3) == 0) {
-		console.log('fizzbuzz')
+	}
+	else {
+		console.log(i)
 	};
 };
+};
 
+playFizzbuzz(num);
